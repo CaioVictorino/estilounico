1318573fb5e59f53cdc20ddd495d9325875b7cfe
Caio Victorino <caiodeveloper@hotmail.com>
2024-01-13 22:26:05 -0300

    base

--- views/snippets/header.php
+++ views/snippets/header.php
@@ -4,11 +4,7 @@
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Portal Dealer - VAG Especialista</title>
-
-     <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-     <link rel="stylesheet" href="/assets/css/adminlte.min.css">
-     <link rel="stylesheet" href="/assets/css/custom.css">
-     <link rel="stylesheet" href="/assets/css/datatable.css">
+     <title>Estilo Único</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
      <link rel="icon" type="image/x-icon" href="storage/images/favicon.ico">
 </head>
